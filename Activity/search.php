43b96938a3c72a07101486ccbe34e6a5cb19fb6c
Camil Grace Bumatay <cjb4899@students.uc-bcf.edu.ph>
2024-10-13 20:24:10 +0800

    Fix and updates the code

--- Activity/search.php
+++ Activity/search.php
@@ -6,10 +6,10 @@
     <title>PHP Database Search</title>
 </head>
 <body>
-    <h1>Search Books in the Database</h1>
+    <h2>Search Books in the Database</h2>
 
-    <form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-        <label for="search">Search fr a book title:</label>
+    <form method="get" action="search.php">
+        <label for="search">Search for a book title:</label>
         <br>
         <input type="text" name="query" id="search">
         <br><br>
@@ -18,36 +18,45 @@
     </form>
 
     <?php
-    if (isset($_GET['query'])) {
-        $searchQuery = htmlspecialchars($_GET['query']);
+   
+    echo "Search Query: " . $searchQuery;
 
-        $servername = "localhost";
-        $username = "camil";
-        $password = "camilgrace";
-        $dbname = "search_db";
+        if (isset($_GET['query'])) {
+            $searchQuery = htmlspecialchars($_GET['query']);
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+            $servername = "localhost";
+            $username = "";
+            $password = "";
+            $dbname = "search_db";
 
-        if ($conn->connect_error) {
-            die("Connection failed: ". $conn->connect_error);
-        }
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
-        $sql = "SELECT * FROM books WHERE title LIKE '%$searchQuery%'";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            echo "<h2>Search Results:</h2>";
-            echo "<ul>";
-            while ($row = $result->fetch_assoc()) {
-                echo "<li>" . $row["title"] . " by " .$row["author"] . " (" .$row["year_published"] . ")</li>";
+            if ($conn->connect_error) {
+                die("Connection failed: ". $conn->connect_error);
+            }else{
+                echo "Connected successfully!";
             }
-            echo "</ul>";
-        } else {
-            echo "No results found for: " . $searchQuery;
-        }
 
-        $conn->close();
-    }
+            $sql = "SELECT * FROM books WHERE LOWER(title) LIKE LOWER('%$searchQuery%')";
+            if (!$result = $conn->quesry($sql)) {
+                echo "SQL error: " . $conn->error;
+            }
+
+            $result = $conn->query($sql);
+
+            if ($result->num_rows > 0) {
+                echo "<h2>Search Results:</h2>";
+                echo "<ul>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<li>" . $row["title"] . " by " .$row["author"] . " (" .$row["year_published"] . ")</li>";
+                }
+                echo "</ul>";
+            } else {
+                echo "No results found for: " . $searchQuery;
+            }
+
+            $conn->close();
+        };
     ?>
 
 </body>
