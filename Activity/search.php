28588a004be32d3d2a1090fe956133fb6efd415d
Camil Grace Bumatay <cjb4899@students.uc-bcf.edu.ph>
2024-10-13 20:54:49 +0800

    Updates the code

--- Activity/search.php
+++ Activity/search.php
@@ -8,7 +8,7 @@
 <body>
     <h2>Search Books in the Database</h2>
 
-    <form method="get" action="search.php">
+    <form method="get" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label for="search">Search for a book title:</label>
         <br>
         <input type="text" name="query" id="search">
@@ -18,37 +18,37 @@
     </form>
 
     <?php
-    if (isset($_GET['query'])) {
-        $searchQuery = htmlspecialchars($_GET['query']);
+        if (isset($_GET['query'])) {
+            $searchQuery = htmlspecialchars($_GET['query']);
 
-        $servername = "localhost";
-        $username = "root";
-        $password = "camilgrace";
-        $dbname = "search_db";
+            $servername = "localhost";
+            $username = "root";
+            $password = "camilgrace";
+            $dbname = "search_db";
 
-        $conn = new mysqli($servername, $username, $password, $dbname);
+            $conn = new mysqli($servername, $username, $password, $dbname);
 
-        if ($conn->connect_error) {
-            die("Connection failed: ". $conn->connect_error);
-        }
-        
-        $sql = "SELECT * FROM books WHERE title LIKE '%$searchQuery%'";
-        $result = $conn->query($sql);
-
-        if ($result->num_rows > 0) {
-            echo "<h2>Search Results:</h2>";
-            echo "<ul>";
-            
-            while ($row = $result->fetch_assoc()) {
-                echo "<li>" . $row["title"] . " by " .$row["author"] . " (" .$row["year_published"] . ")</li>";
+            if ($conn->connect_error) {
+                die("Connection failed: ". $conn->connect_error);
             }
-            echo "</ul>";
-        } else {
-            echo "No results found for: " . $searchQuery;
-        }
+            
+            $sql = "SELECT * FROM books WHERE title LIKE '%$searchQuery%'";
+            $result = $conn->query($sql);
 
-        $conn->close();
-    };
+            if ($result->num_rows > 0) {
+                echo "<h2>Search Results:</h2>";
+                echo "<ul>";
+                
+                while ($row = $result->fetch_assoc()) {
+                    echo "<li>" . $row["title"] . " by " .$row["author"] . " (" .$row["year_published"] . ")</li>";
+                }
+                echo "</ul>";
+            } else {
+                echo "No results found for: " . $searchQuery;
+            }
+
+            $conn->close();
+        };
     ?>
 
 </body>
